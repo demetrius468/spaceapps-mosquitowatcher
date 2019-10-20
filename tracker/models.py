f970158425c55178a98b875d60b288c8d4b8dbda
Matheus Santos <tetofsa15@gmail.com>
2019-10-19 23:53:58 -0300

    modelo Bairro

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Bairro(models.Model):
+    n_pessoas = models.IntegerField('Número de pessoas')
+    n_criancas_1 = models.IntegerField('Número de crianças com menos de 1 ano')
+    n_idosos = models.IntegerField('Número de idosos')
+    
+    @property
+    def n_gravidas(self):
+        return n_criancas_1
